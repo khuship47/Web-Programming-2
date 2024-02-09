f4b7119dd497eae42af7cfc893526af321bfc783
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-02-09 12:38:31 -0500

    Add files via upload

---
+++ lab11b/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderApp from './components/HeaderApp.js';
+
+function App() {
+  return (
+    <main> 
+      <HeaderApp />
+    </main>
+  );
+}
+
+export default App;
---
+++ lab11b/my-app/src/components/HeaderApp.js
@@ -0,0 +1,13 @@
+import HeaderBar from './HeaderBar.js';
+import HeaderMenu from './HeaderMenu.js';
+
+
+const HeaderApp = function (props) { 
+    return (
+        <header className="header"> 
+            <HeaderBar />
+            <HeaderMenu />
+        </header>
+    ); 
+}
+export default HeaderApp;
