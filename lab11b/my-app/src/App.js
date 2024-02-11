79acaf5fa4d8fd9d96aecb270b54fdd99f93794b
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-02-11 17:03:11 -0500

    Exercise-11b-7&8: Done

--- lab11b/my-app/src/App.js
+++ lab11b/my-app/src/App.js
@@ -1,12 +1,53 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import React from "react";
+//import React from "react";
+import React, { useEffect, useState } from 'react';
 import HeaderApp from './components/HeaderApp.js';
-import PhotoThumb from './components/PhotoThumb.js';
+//import PhotoThumb from './components/PhotoThumb.js';
 import PhotoBrowser from './components/PhotoBrowser.js';
 
 
+function App() {
+  
+  const [photos, setPhotos] = useState([]);
 
+  /*
+  useEffect( () => { 
+    const url = "https://www.randyconnolly.com/funwebdev/3rd/api/travel/images.php?iso=gb";
+    fetch(url)
+      .then( resp => resp.json() )
+      .then( data => setPhotos(data)) 
+      .catch( err => console.error(err));
+  });
+  */
+
+  useEffect( () => {
+    const getData = async () => {
+
+      try {
+        const url = "https://www.randyconnolly.com/funwebdev/3rd/api/travel/images.php?iso=gb";
+        const response = await fetch(url); const data = await response.json(); setPhotos(data);
+      }
+      catch (err) {
+        console.error(err); 
+      }
+    };
+    // invoke the async function
+    getData(); 
+  }, [] );
+
+  return ( 
+    <main>
+    <HeaderApp />
+    <PhotoBrowser photos={photos} /> 
+    </main>
+  );
+}
+  
+export default App;
+
+
+/*
 class App extends React.Component { 
 
   constructor(props) { 
@@ -34,6 +75,7 @@ class App extends React.Component {
   }
 }
 export default App;
+*/
 
 /*
 function App() {
