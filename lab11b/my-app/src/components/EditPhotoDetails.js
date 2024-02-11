5bd7e6f35abd8cdbe3a58f1639233dc2d3b32bf8
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-02-11 18:34:43 -0500

    Exercise-11b.11: Done.

--- lab11b/my-app/src/components/EditPhotoDetails.js
+++ lab11b/my-app/src/components/EditPhotoDetails.js
@@ -3,23 +3,34 @@ import './EditPhotoDetails.css';
 
 class EditPhotoDetails extends React.Component { 
     render() {
-        const imgURL = `https://www.randyconnolly.com/funwebdev/3rd/images/travel/medium640/5855174537.jpg`;
-        return (
-            <article className="details">
-                <div className="detailsPhotoBox"> 
-                    <form className="photoForm">
-                        <legend>Edit Photo Details</legend> 
-                        <img src={imgURL} alt="later" />
-                        <label>Title</label>
-                        <input type='text' name='title' />
-                        <label>City</label>
-                        <input type='text' name='city' />
-                        <label>Country</label>
-                        <input type='text' name='country' /> 
-                    </form>
-                </div> 
-            </article>
-        ); 
+        const id = this.props.currentPhoto;
+        const imgURL = `https://www.randyconnolly.com/funwebdev/3rd/images/travel/medium640/`;
+           // just in case, handle case of a missing photos collection
+        if (this.props.photos.length > 0) {
+              // find the photo object with this id
+            const photo = this.props.photos.find( p => p.id === id);
+              // just in case there is no match, then exit
+        if (! photo) return null;
+              // we have found a photo for this id, so display edit form
+            return (
+                <article className="details">
+                    <div className="detailsPhotoBox">
+                        <form className="photoForm">
+                            <legend>Edit Photo Details</legend>
+                            <img src={imgURL+photo.filename} alt={photo.title} />
+                            <label>Title</label>
+                            <input type='text' name='title' value={photo.title} />
+                            <label>City</label>
+                            <input type='text' name='city' value={photo.location.city} />
+                            <label>Country</label>
+                            <input type='text'  name='country' value={photo.location.country} />      
+                        </form>
+                    </div>
+                </article>
+            );
+        } else {
+            return null;
+        }
     }
 }
 export default EditPhotoDetails;
--- lab11b/my-app/src/components/PhotoBrowser.js
+++ lab11b/my-app/src/components/PhotoBrowser.js
@@ -1,11 +1,15 @@
+import React, { useEffect, useState } from 'react';
 import PhotoList from './PhotoList.js';
 import EditPhotoDetails from './EditPhotoDetails.js';
 
-const PhotoBrowser = props => { 
+const PhotoBrowser = props => {
+    // the first photo in fetched data has an id of 15
+    const [currentPhoto, setCurrentPhoto] = React.useState(15); 
+
     return (
         <section className="container"> 
             <PhotoList photos={props.photos}/> 
-            <EditPhotoDetails photos={props.photos}/>
+            <EditPhotoDetails photos={props.photos} currentPhoto={currentPhoto}/>
         </section> 
     );
 }
