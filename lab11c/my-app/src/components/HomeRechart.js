a15862254d2a11e7d0cb26076b26d007b1a9ff37
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-02-23 20:57:03 -0500

    Exercise-1: done

---
+++ lab11c/my-app/src/components/HomeRechart.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomeRechart = (props) => {
+
+  const data = [
+    {
+      "name": "MSFT",
+      "2018": 4000,
+      "2019": 2400,
+      "2020": 2530
+    },
+    {
+      "name": "AMZN",
+      "2018": 3000,
+      "2019": 1398,
+      "2020": 1200
+    },
+    {
+      "name": "APPL",
+      "2018": 2000,
+      "2019": 9800,
+      "2020": 2400
+    },
+    {
+      "name": "FB",
+      "2018": 4000,
+      "2019": 6200,
+      "2020": 5500
+    }    
+  ];
+
+  return (
+   <section>
+     Home Rechart Demo
+   </section>
+  );
+
+};
+
+export default HomeRechart;
