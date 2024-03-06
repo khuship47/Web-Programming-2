80e0b988d480f4d401351a98e88008903110a65d
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-03-06 17:51:44 -0500

    Add files via upload

---
+++ lab11b/my-app/src/components/HeaderApp.js
@@ -0,0 +1,12 @@
+import HeaderBar from './HeaderBar.js';
+import HeaderMenu from './HeaderMenu.js';
+
+const HeaderApp = function (props) { 
+    return (
+        <header className="header"> 
+            <HeaderBar />
+            <HeaderMenu />
+        </header>
+    ); 
+}
+export default HeaderApp;
