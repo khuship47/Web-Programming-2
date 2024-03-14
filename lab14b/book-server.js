016f4554bd2c6d3eab1d3a74b657109ffc4ea4a7
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-03-14 17:50:32 -0400

    lab-14b.9: Done

---
+++ lab14b/book-server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const opt = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+console.log("starting to connect to mongo ...");
+mongoose.connect(process.env.MONGO_URL, opt);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open', () => {
+    console.log("connected to mongo");
+});
