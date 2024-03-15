d0ae1827edfae75207e5e629381e76e20b44821d
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-03-15 17:36:56 -0400

    exercise-14b.13: done

--- lab14b/book-server.js
+++ lab14b/book-server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const Book = require('./models/Book');
 
+// serves up static files from the public folder.
+app.use(express.static('public'));
+// also add a path to static
+app.use('/static', express.static('public'));
+
 // tell node to use json and HTTP header features in body-parser
 app.use(express.urlencoded({extended: true}));
 // use the route handlers
@@ -13,6 +18,11 @@ bookRouter.handleBooksByPageRange(app, Book);
 
 // create connection to database
 require('./handlers/dataConnector.js').connect();
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log("Server running at port= " + port);
