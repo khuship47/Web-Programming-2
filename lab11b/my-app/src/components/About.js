d03969d96f1df6c9217b6c8292983a784125bb5d
khuship47 <98182634+khuship47@users.noreply.github.com>
2024-04-08 19:19:08 -0400

    exercise-15.6:Done

--- lab11b/my-app/src/components/About.js
+++
@@ -1,9 +0,0 @@
-const About = function (props) { 
-    return (
-        <div className="header-titles"> 
-            <h1>About</h1>
-            <p>You can make this more fancy</p> 
-        </div>
-    );
-}
-export default About;
